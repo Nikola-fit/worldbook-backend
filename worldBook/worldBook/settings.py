2a8ea715731cb805d385541ecda52ed643fdd012
Nikola Lalicicm <nikolalalicic5@gmail.com>
2026-04-28 22:51:27 +0200

    "STATICFILES_FIRS added"

--- worldBook/worldBook/settings.py
+++ worldBook/worldBook/settings.py
@@ -173,6 +173,8 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+STATICFILES_DIRS = []
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
