cafa1071443ffc08f91e394770ff94273ac1a781
Nikola Lalicicm <nikolalalicic5@gmail.com>
2026-04-28 22:58:16 +0200

    add whitenoise for static files

--- worldBook/worldBook/settings.py
+++ worldBook/worldBook/settings.py
@@ -88,6 +88,7 @@ REST_FRAMEWORK = {
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
