0181fd32ee023a1c026da2b0ca38a5375425089c
Nikola Lalicicm <nikolalalicic5@gmail.com>
2026-05-23 00:05:48 +0200

    fix registration url trailing slash

--- worldBook/user/api/urls.py
+++ worldBook/user/api/urls.py
@@ -7,7 +7,7 @@ from rest_framework_simplejwt.views import (
 from .serializers import CustomTokenObtainPairView
 
 urlpatterns = [
-    path("registration", registration_view, name="registration"),
+    path("registration/", registration_view, name="registration"),
     path("api/token/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
